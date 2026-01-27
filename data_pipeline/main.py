8145de1a867fc63a0ce512419489e5cca2f58ea2
Ricardo Picinatto <ederli.picinatto@gmail.com>
2026-01-27 06:39:23 -0300

    add uv to Dockerfile

--- data_pipeline/main.py
+++ data_pipeline/main.py
@@ -1,7 +1,9 @@
 from src import data_pipeline
 
+
 def main():
     data_pipeline.execute()
+
 
 if __name__ == "__main__":
     main()
--- data_pipeline/src/data_pipeline.py
+++ data_pipeline/src/data_pipeline.py
@@ -1,8 +1,7 @@
 import sys
 
-import pandas as pd
 
 def execute():
     month = int(sys.argv[1])
-    
+
     print("Executing data pipeline for month:", month)
