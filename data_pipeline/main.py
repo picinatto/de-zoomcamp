aa13520aa02c7f3752569602754fef6d7ac8c9bb
Ricardo Picinatto <ederli.picinatto@gmail.com>
2026-01-26 21:49:20 -0300

    add pipeline

---
+++ data_pipeline/main.py
@@ -0,0 +1,7 @@
+from src import data_pipeline
+
+def main():
+    data_pipeline.execute()
+
+if __name__ == "__main__":
+    main()
---
+++ data_pipeline/src/data_pipeline.py
@@ -0,0 +1,8 @@
+import sys
+
+import pandas as pd
+
+def execute():
+    month = int(sys.argv[1])
+    
+    print("Executing data pipeline for month:", month)
